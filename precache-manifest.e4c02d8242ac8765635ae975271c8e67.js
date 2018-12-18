505fe40455a4240832ce7c01d4b22a5bcad2badb
jrmrsr <jjr_go@outlook.com>
2018-12-17 21:42:55 -0800

    Updates

---
+++ precache-manifest.e4c02d8242ac8765635ae975271c8e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c81a8d0f7dd8d5860ea9",
+    "url": "/Everyday-Calendar/static/js/runtime~main.c81a8d0f.js"
+  },
+  {
+    "revision": "7b3a8c1a8e9adf992ea6",
+    "url": "/Everyday-Calendar/static/js/main.7b3a8c1a.chunk.js"
+  },
+  {
+    "revision": "fd4896fb8da2be61bb4a",
+    "url": "/Everyday-Calendar/static/js/1.fd4896fb.chunk.js"
+  },
+  {
+    "revision": "7b3a8c1a8e9adf992ea6",
+    "url": "/Everyday-Calendar/static/css/main.711a6d51.chunk.css"
+  },
+  {
+    "revision": "3caea348378a3e878cf5795168b680f9",
+    "url": "/Everyday-Calendar/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edfd9d22d5d2c0cb5b2134dc4cc42dd6.js"
+  "/Everyday-Calendar/precache-manifest.e4c02d8242ac8765635ae975271c8e67.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Everyday-Calendar/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
