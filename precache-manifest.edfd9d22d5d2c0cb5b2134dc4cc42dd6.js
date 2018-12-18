48214f39f0f455a0fca6999c4646756e1fa13586
jrmrsr <jjr_go@outlook.com>
2018-12-17 21:41:56 -0800

    Updates

---
+++ precache-manifest.edfd9d22d5d2c0cb5b2134dc4cc42dd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ca3258bf7223f100bdf",
+    "url": "/static/js/main.1ca3258b.chunk.js"
+  },
+  {
+    "revision": "fd4896fb8da2be61bb4a",
+    "url": "/static/js/1.fd4896fb.chunk.js"
+  },
+  {
+    "revision": "1ca3258bf7223f100bdf",
+    "url": "/static/css/main.711a6d51.chunk.css"
+  },
+  {
+    "revision": "d137daae34f9730a6dea09a1b35c65bb",
+    "url": "/index.html"
+  }
+];
